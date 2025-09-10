8c993ccff9f27807fd8e9abddc2002a3da516487
architectingalex <alex.paluch@yahoo.com>
2025-09-10 19:06:22 +0100

    Enhanced Input

--- Source/GameAttempt07/Public/Character/GameCharacterBase.h
+++ Source/GameAttempt07/Public/Character/GameCharacterBase.h
@@ -16,6 +16,7 @@ public:
 
 protected:
 	virtual void BeginPlay() override;
-	
+
+
 
 };
---
+++ Source/GameAttempt07/Public/Player/GamePlayerController.h
@@ -0,0 +1,17 @@
+// Copyright Big Rocket Games
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerController.h"
+#include "GamePlayerController.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class GAMEATTEMPT07_API AGamePlayerController : public APlayerController
+{
+	GENERATED_BODY()
+	
+};
